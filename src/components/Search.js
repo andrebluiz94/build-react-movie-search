75305b33e5aefdecfada1a4a0e2997f48514190b
Andre luiz <andre.bluiz94@gmail.com>
2020-03-03 15:01:27 -0300

    add component Search

---
+++ src/components/Search.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+const Search = ( props ) => {
+    const [searchValue, setSearchValue] = useState( "" );
+
+    const handleSearchInputChanges = ( e ) => {
+        setSearchValue( e.target.value );
+    }
+
+    const resetInputField = () => {
+        setSearchValue( "" )
+    }
+
+    const callSearchFunction = ( e ) => {
+        e.preventDefault();
+        props.search( searchValue )
+        resetInputField()
+    }
+
+    return (
+        <form className="search">
+            <input
+                type="text"
+                value={searchValue}
+                onChange={handleSearchInputChanges}
+            />
+            <input
+                type="text"
+                onClick={callSearchFunction}
+                type="submit"
+                value="SEARCH"
+            />
+        </form>
+    )
+}
+
+export default Search;
